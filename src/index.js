c744a9611e1863e3c375ccf701e4de1c69087ca1
zelle018 <llanasasjayzel30@gmail.com>
2022-01-20 16:31:00 +0800

    Add some CSS styles and implement inline style

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import "./index.css";
 
 function App() {
 
+	const paraStyle ={
+		"marginBottom" : "15px"
+	};
+
 	return (
 
 		<div className="app">
@@ -14,7 +18,13 @@ function App() {
 
 				<h1>My Amazing ToDo-List App</h1>
 
-				<p>The most simple and amazing todo-list React App</p>
+				<p style={paraStyle}>The most simple and amazing todo-list React App</p>
+
+				<ul>
+					<li className="tasks-item">Follow Edukasyon.ph on Facebook</li>
+					<li className="tasks-item">Follow AWS Siklab-Pilipinas on Facebook</li>
+					<li className="tasks-item">Follow Zuiit Coding Bootcamp on facebook</li>
+				</ul>
 
 			</header>
 
